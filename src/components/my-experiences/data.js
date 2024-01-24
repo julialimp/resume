1bd2ed5ae11e38a4387668351362756afa2394df
Julia Limp <julia.limp@hotmail.com>
2024-01-24 16:05:21 -0300

    adjusted  "my experiences" and "portfolio" page texts

--- src/components/my-experiences/data.js
+++ src/components/my-experiences/data.js
@@ -7,7 +7,7 @@ const items = [
         items: [
             {
                 cardTitle: "Co-owner",
-                cardDetailedText: "I worked at a cafe for one year (from 2018 to 2019) as a barista and serving clientes. I fell in love with Specialty coffee and decided to open my own café. There I did a little bit of everything, focusing on the customer experience. Dealing with clients was something I liked and I was already used to from my previous experiences. I could improve a lot of other skills I already had, such as organization, management, finances, problem-solving, etc. I had to deal with a lot of different situations and had daily contact with clients and suppliers. I could grow a lot, both personally and professionally, with this huge experience, but the time came where I realized this cycle had come to an end and change careers.",
+                cardDetailedText: "I worked at a cafe for one year (from 2018 to 2019) as a barista and serving clientes. I fell in love with Specialty coffee and decided to open my own café. There I did a little bit of everything, focusing on the management and customer experience, attending 50-100 clients a day. Dealing with clients and undesrtanding their needs is something I really like and was already used to from my previous experiences. I could improve a lot of other skills I already had, such as organization, management, finances, problem-solving, and I grew up a lot both personally and professionally with this experience.",
             }
         ]
     },
@@ -25,12 +25,12 @@ const items = [
             {
                 cardTitle: "Assistant Administrative",
                 cardSubtitle: "Feb 2014",
-                cardDetailedText: "In 2014 I was hired as an administrative assistant, still in the tariff area at the beginning, and later, in 2015, besides the tariff sector I started also being responsible for the cargo insurance. In that area I had constant contact with both  inside and outside customers. I was responsible for the customer support when needed to activate the cargo insurance, that part was very delicate because I had to bring the bad news about the damaged cargo, so it required a good communication skill. I had to deal with problem solving and monthly reports. Since I had to manage two areas at the same time, I could improve abilities such as time management, organization, process optimization, and so on."
+                cardDetailedText: "In 2014 I was hired as an administrative assistant and in 2015, besides the tariff sector I started also being responsible for the cargo insurance. In that area I had constant contact with both inside and outside customers. I was responsible for the customer support when needed to activate the cargo insurance, that part was very delicate because I had to bring the bad news about the damaged cargo, so it required a good communication skill and empathy. I had to deal with problem solving and monthly reports. Since I had to manage two areas at the same time, I could improve abilities such as time management, organization, process optimization, and so on."
             },
             {
                 cardTitle: "Trainee",
                 cardSubtitle: "Feb 2013",
-                cardDetailedText: "At Kuehne + Nagel I started in 2013 as a trainee in the tariff sector. I was responsible for registering the freight negotiation values in the system for later issuance of the document for transportation.",
+                cardDetailedText: "At Kuehne + Nagel I started in 2013 as an intern in the tariff sector. I was responsible for registering the freight negotiation values in the system for later issuance of the document for transportation.",
             },
         ]
     },
@@ -43,12 +43,12 @@ const items = [
             {
                 cardTitle: "Assistant Administrative",
                 cardSubtitle: "May 2012",
-                cardDetailedText: "When I was contracted as an assistant I was responsible for the renovation sector, where I contacted customers one week prior to the insurance expiration date to update and confirm their data, then I distributed the proposals to the team and also did some quotations and then passed them on to the sales team. ",
+                cardDetailedText: "When I was contracted as an assistant I was responsible for the renovation sector, where I contacted customers one week prior to the insurance expiration date to update and confirm their data, then I organized and distributed the proposals to the team and also did some quotations and then passed them on to the sales team.",
             },
             {
                 cardTitle: "Trainee",
                 cardSubtitle: "Jan 2012",
-                cardDetailedText: "As a trainee, I was responsible for contacting new customers to collect data so that I could have all the information needed to calculate the insurance.",
+                cardDetailedText: "As an intern, I was responsible for contacting new customers to collect data so that I could have all the information needed to calculate the insurance.",
             },
         ]
     }
