b876b40f07f332af16c1d10ca007503a4d5ee722
Julia Limp <julia.limp@hotmail.com>
2024-01-25 19:36:11 -0300

    media query curriculum page

--- src/styles/cv-style.js
+++ src/styles/cv-style.js
@@ -13,12 +13,21 @@ export const Div = styled.div`
     max-height: 80vh;
     margin: 50px;
 
-p {
-    text-transform: uppercase;
-    text-align: center;
-    font-weight: bold;
-    letter-spacing: 4px;
-    padding: 15px;
-    color: ${(props) => props.theme.color};
-}
+    @media (max-width: 600px) {
+        scale: .9;
+        margin: 0;
+    }
+
+    @media (max-width: 500px) {
+        scale: .7;
+    }
+
+    p {
+        text-transform: uppercase;
+        text-align: center;
+        font-weight: bold;
+        letter-spacing: 4px;
+        padding: 15px;
+        color: ${(props) => props.theme.color};
+    }
 `
